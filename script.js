830e588753f5f8414bbecdb102fe0e91b25aad48
huzaifa1993 <huothyfayusuf1993@gmail.com>
2023-03-21 14:11:46 +0000

    create nav element using for loop

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
 const menuBars = document.getElementById('menu-bars');
 const overlay = document.getElementById('overlay');
-const nav1 = document.getElementById('nav-1');
-const nav2 = document.getElementById('nav-2');
-const nav3 = document.getElementById('nav-3');
-const nav4 = document.getElementById('nav-4');
-const nav5 = document.getElementById('nav-5');
-const navItems = [nav1, nav2, nav3, nav4, nav5];
+const navElement = document.getElementById('nav-ul');
 
- function navAnimation (direction1 ,direction2) {
+const navTabs = ["HOME", "ABOUT", "SKILLS", "PROJECTS", "CONTACT"]
+let navItems = []
+for (let i = 0; i < navTabs.length; i++) {
+    let liElement = document.createElement("li")
+    liElement.id = `nav-${i + 1}`
+    navElement.append(liElement)
+    let aElement = document.createElement("a")
+    liElement.append(aElement)
+    aElement.innerHTML = navTabs[i]
+    aElement.href = `#${navTabs[i]}`.toLowerCase()
+    navItems.push(aElement)
+    // liElement.classList.add(`slide-out-${i+1}`)
+
+}
+
+
+function navAnimation(direction1, direction2) {
     navItems.forEach((nav, i) => {
-        nav.classList.replace(`slide-${direction1}-${i +1}`,`slide-${direction2}-${i +1}`)
+        console.log(nav);
+        nav.classList.replace(`slide-${direction1}-${i + 1}`, `slide-${direction2}-${i + 1}`)
     });
- }
+}
 
 function toggleNav() {
-    menuBars.classList.toggle('change');  
+    menuBars.classList.toggle('change');
     overlay.classList.toggle('overlay-active');
-if (overlay.classList.contains('overlay-active')) {
-    overlay.classList.replace('overlay-slide-left','overlay-slide-right');
-    navAnimation ('out', 'in');
+    if (overlay.classList.contains('overlay-active')) {
+        overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
+        navAnimation('out', 'in');
 
-} else {
-    overlay.classList.replace('overlay-slide-right','overlay-slide-left');
-    navAnimation ('in', 'out');
- }
+    } else {
+        overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
+        navAnimation('in', 'out');
+    }
 }
 
 menuBars.addEventListener('click', toggleNav);
 navItems.forEach((nav) => {
+    console.log(nav);
     nav.addEventListener('click', toggleNav);
 });
+
+
+
 
